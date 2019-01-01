ad193de090f3ad471826c79775058e7510aa7a43
vtorkalo <torkalo@gmail.com>
2019-01-01 18:26:17 +0200

    Iterative search in tree (without recursion) Using stack for deep first and queue for width first

---
+++ Tree_search_without_recursion/Tree_search_without_recursion/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Depth_first_search_Iterative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for depth first search
+            var container = new QueueContainer<string>(); 
+
+            // for width first search
+            //var container = new StackContainer<string>();
+
+            container.Add(@"d:\");
+            var allNodes = new List<string>();
+
+            while (container.Any())
+            {
+                var currentDir = container.GetAndRemove();
+                allNodes.Add(currentDir);
+                Console.WriteLine(currentDir);
+                try
+                {
+                    var directories = Directory.GetDirectories(currentDir).AsEnumerable();
+                    if (container is StackContainer<string>)
+                    {
+                        directories = directories.Reverse();
+                    }
+
+                    foreach (var subnode in directories)
+                    {
+                        container.Add(subnode);
+                    }
+                }
+                catch (Exception) { }
+            }
+
+            foreach (var node in allNodes)
+            {
+                Console.WriteLine(node);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface IDataContainer<T>
+    {
+        T GetAndRemove();
+        void Add(T item);
+        bool Any();
+    }
+
+    public class StackContainer<T> : IDataContainer<T>
+    {
+        private Stack<T> _stack = new Stack<T>();
+
+        public bool Any()
+        {
+            return _stack.Any();
+        }
+
+        public T GetAndRemove()
+        {
+            return _stack.Pop();
+        }
+
+        public void Add(T item)
+        {
+            _stack.Push(item);
+        }
+    }
+
+    public class QueueContainer<T> : IDataContainer<T>
+    {
+        private Queue<T> _queue = new Queue<T>();
+
+        public bool Any()
+        {
+            return _queue.Any();
+        }
+
+        public T GetAndRemove()
+        {
+            return _queue.Dequeue();
+        }
+
+        public void Add(T item)
+        {
+            _queue.Enqueue(item);
+        }
+    }
+}
