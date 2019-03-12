583eb02bdda0ad239806cb9522164d8798b675e5
vtork <vtork@softserveinc.com>
2019-03-12 20:58:36 +0200

    1

---
+++ Checkers/Checkers.Core/Field.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkers.Core
+{
+    public static class Field
+    {
+        public static int GetCellIndex(int row, int col)
+        {
+            int index = row * 4 + (col + row % 2 )/2;
+            return index;
+        }
+
+        public static void GetRowCol(int index, out int row, out int col)
+        {
+            row = index / 4;
+            col = (index % 4) * 2 + 1 - row % 2;
+        }
+
+        public static CellState GetValue(CellState[] field, Cell cell)
+        {
+            return GetValue(field, cell.Row, cell.Col);
+        }
+
+        public static CellState GetValue(CellState[] field, int row, int col)
+        {            
+            int index = GetCellIndex(row, col);
+            return field[index];
+        }
+
+        public static void SetValue(CellState[] field, int row, int col, CellState value)
+        {
+            int index = GetCellIndex(row, col);
+            field[index] = value;
+        }
+
+        public static void SetValue(CellState[] field, Cell cell, CellState value)
+        {
+            SetValue(field, cell.Row, cell.Col, value);
+        }
+    }
+}
