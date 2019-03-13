f434064804ac6178a396cbcf689c4f40bff1b0cf
vtorkalo <torkalo@gmail.com>
2019-03-13 11:28:28 +0200

    assert fix

--- Checkers/Checkers.Core.Tests/StandartPathGeneratorTests.cs
+++ Checkers/Checkers.Core.Tests/StandartPathGeneratorTests.cs
@@ -24,7 +24,7 @@ namespace Checkers.Core.Tests
             var expected = new List<Move> { new Move { new Cell(6, 3), new Cell(5, 2), new Cell(4, 1) }, new Move { new Cell(6, 3), new Cell(5, 4), new Cell(4, 5), new Cell(3, 4), new Cell(2, 3), new Cell(1, 4), new Cell(0, 5), new Cell(1, 6), new Cell(2, 7), new Cell(3, 6), new Cell(4, 5), new Cell(5, 6), new Cell(6, 7) }, new Move { new Cell(6, 3), new Cell(5, 4), new Cell(4, 5), new Cell(3, 6), new Cell(2, 7), new Cell(1, 6), new Cell(0, 5), new Cell(1, 4), new Cell(2, 3), new Cell(3, 4), new Cell(4, 5), new Cell(5, 6), new Cell(6, 7) }, new Move { new Cell(6, 3), new Cell(5, 4), new Cell(4, 5), new Cell(5, 6), new Cell(6, 7) } };
 
 
-            Assert.IsTrue(actual.SequenceEqual(expected, _pathComparer));
+            AreEquivalent(actual, expected);
         }
 
         [TestMethod]
@@ -33,9 +33,9 @@ namespace Checkers.Core.Tests
             var field = TestFieldData.Standart_Moves_Case2();
             var actual = _target.GetPossibleMovements(field, new Cell(7, 2));
             var expected = new List<Move>();
-  
 
-            Assert.IsTrue(actual.SequenceEqual(expected, _pathComparer));
+
+            AreEquivalent(actual, expected);
         }
 
         [TestMethod]
@@ -45,7 +45,7 @@ namespace Checkers.Core.Tests
             var actual = _target.GetPossibleMovements(field, new Cell(5, 4));
             var expected = new List<Move> { new Move { new Cell(5, 4), new Cell(4, 3) }, new Move { new Cell(5, 4), new Cell(4, 5) } };
 
-            Assert.IsTrue(actual.SequenceEqual(expected, _pathComparer));
+            AreEquivalent(actual, expected);
         }
 
         [TestMethod]
@@ -55,7 +55,7 @@ namespace Checkers.Core.Tests
             var actual = _target.GetPossibleMovements(field, new Cell(4, 3));
             var expected = new List<Move> { new Move { new Cell(4, 3), new Cell(3, 4), new Cell(2, 5) } };
 
-            Assert.IsTrue(actual.SequenceEqual(expected, _pathComparer));
+            AreEquivalent(actual, expected);
         }
 
         [TestMethod]
@@ -65,7 +65,12 @@ namespace Checkers.Core.Tests
             var actual = _target.GetPossibleMovements(field, new Cell(7, 0));
             var expected = new List<Move> { new Move { new Cell(7, 0), new Cell(6, 1), new Cell(5, 2), new Cell(6, 3), new Cell(7, 4), new Cell(6, 5), new Cell(5, 6), new Cell(4, 5), new Cell(3, 4), new Cell(4, 3), new Cell(5, 2), new Cell(4, 1), new Cell(3, 0), new Cell(2, 1), new Cell(1, 2), new Cell(2, 3), new Cell(3, 4), new Cell(2, 5), new Cell(1, 6) }, new Move { new Cell(7, 0), new Cell(6, 1), new Cell(5, 2), new Cell(6, 3), new Cell(7, 4), new Cell(6, 5), new Cell(5, 6), new Cell(4, 5), new Cell(3, 4), new Cell(2, 3), new Cell(1, 2), new Cell(2, 1), new Cell(3, 0), new Cell(4, 1), new Cell(5, 2), new Cell(4, 3), new Cell(3, 4), new Cell(2, 5), new Cell(1, 6) }, new Move { new Cell(7, 0), new Cell(6, 1), new Cell(5, 2), new Cell(6, 3), new Cell(7, 4), new Cell(6, 5), new Cell(5, 6), new Cell(4, 5), new Cell(3, 4), new Cell(2, 5), new Cell(1, 6) }, new Move { new Cell(7, 0), new Cell(6, 1), new Cell(5, 2), new Cell(4, 1), new Cell(3, 0), new Cell(2, 1), new Cell(1, 2), new Cell(2, 3), new Cell(3, 4), new Cell(4, 3), new Cell(5, 2), new Cell(6, 3), new Cell(7, 4), new Cell(6, 5), new Cell(5, 6), new Cell(4, 5), new Cell(3, 4), new Cell(2, 5), new Cell(1, 6) }, new Move { new Cell(7, 0), new Cell(6, 1), new Cell(5, 2), new Cell(4, 1), new Cell(3, 0), new Cell(2, 1), new Cell(1, 2), new Cell(2, 3), new Cell(3, 4), new Cell(4, 5), new Cell(5, 6), new Cell(6, 5), new Cell(7, 4), new Cell(6, 3), new Cell(5, 2), new Cell(4, 3), new Cell(3, 4), new Cell(2, 5), new Cell(1, 6) }, new Move { new Cell(7, 0), new Cell(6, 1), new Cell(5, 2), new Cell(4, 1), new Cell(3, 0), new Cell(2, 1), new Cell(1, 2), new Cell(2, 3), new Cell(3, 4), new Cell(2, 5), new Cell(1, 6) }, new Move { new Cell(7, 0), new Cell(6, 1), new Cell(5, 2), new Cell(4, 3), new Cell(3, 4), new Cell(4, 5), new Cell(5, 6), new Cell(6, 5), new Cell(7, 4), new Cell(6, 3), new Cell(5, 2), new Cell(4, 1), new Cell(3, 0), new Cell(2, 1), new Cell(1, 2), new Cell(2, 3), new Cell(3, 4), new Cell(2, 5), new Cell(1, 6) }, new Move { new Cell(7, 0), new Cell(6, 1), new Cell(5, 2), new Cell(4, 3), new Cell(3, 4), new Cell(2, 3), new Cell(1, 2), new Cell(2, 1), new Cell(3, 0), new Cell(4, 1), new Cell(5, 2), new Cell(6, 3), new Cell(7, 4), new Cell(6, 5), new Cell(5, 6), new Cell(4, 5), new Cell(3, 4), new Cell(2, 5), new Cell(1, 6) }, new Move { new Cell(7, 0), new Cell(6, 1), new Cell(5, 2), new Cell(4, 3), new Cell(3, 4), new Cell(2, 5), new Cell(1, 6) } };
 
-            Assert.IsTrue(actual.SequenceEqual(expected, _pathComparer));
+            AreEquivalent(actual, expected);
+        }
+
+        private void AreEquivalent(List<Move> actual, List<Move> expected)
+        {
+            Assert.IsTrue(actual.Intersect(expected, _pathComparer).SequenceEqual(actual, _pathComparer));
         }
     }
 }
